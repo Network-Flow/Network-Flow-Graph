f773dd4fe0022229ca55c69533fc8070708c94e8
tsantanam <45335925+tsantanam@users.noreply.github.com>
2019-09-18 22:13:58 -0400

    Create NetworkFlow1.py

---
+++ NetworkFlow1.py
@@ -0,0 +1,130 @@
+# Python program for
+# validation of a graph
+
+# import dictionary for graph
+from collections import defaultdict
+
+# function for adding edge to graph
+graph = defaultdict(list)
+
+
+def addEdge(graph, u, v):
+    graph[u].append(v)
+
+
+# definition of function
+def generate_edges(graph):
+    edges = []
+
+    # for each node in graph
+    for node in graph:
+
+        # for each neighbour node of a single node
+        for neighbour in graph[node]:
+            # if edge exists then append
+            edges.append((node, neighbour))
+    return edges
+
+
+# declaration of graph as dictionary
+addEdge(graph, 'a', 'c')
+addEdge(graph, 'b', 'c')
+addEdge(graph, 'b', 'e')
+addEdge(graph, 'c', 'd')
+addEdge(graph, 'c', 'e')
+addEdge(graph, 'c', 'a')
+addEdge(graph, 'c', 'b')
+addEdge(graph, 'e', 'b')
+addEdge(graph, 'd', 'c')
+addEdge(graph, 'e', 'c')
+
+# Driver Function call
+# to print generated graph
+print(generate_edges(graph))
+
+# Python program to generate the first
+# path of the graph from the nodes provided
+
+graph = {
+    'a': ['c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['a', 'd'],
+    'e': ['b', 'c']
+}
+
+
+# function to find path
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                return newpath
+            return None
+
+
+# Driver function call to print the path
+print(find_path(graph, 'b', 'e'))
+
+# Python program to generate the all possible
+# path of the graph from the nodes provided
+graph = {
+    'a': ['c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['a', 'd'],
+    'e': ['b', 'c']
+}
+
+
+# function to generate all possible paths
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+        for newpath in newpaths:
+            paths.append(newpath)
+    return paths
+
+
+# Driver function call to print all
+# generated paths
+print(find_all_paths(graph, 'b', 'e'))
+
+# Python program to generate shortest path
+
+graph = {
+    'a': ['c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['a', 'd'],
+    'e': ['b', 'c']
+}
+
+
+# function to find the shortest path
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+
+# Driver function call to print
+# the shortest path
+print(find_shortest_path(graph, 'b', 'e'))
